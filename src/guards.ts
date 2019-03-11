5684e2065fd531dbe0d0e0c8a4daaf3d43407351
Jake 'Sid' Smith <jake@dabapps.com>
2019-03-11 22:36:03 +0000

    Add type guards

---
+++ src/guards.ts
@@ -0,0 +1,55 @@
+import {
+  contextSymbol,
+  elementSymbol,
+  fragmentSymbol,
+  MATCHES_CLASS,
+  memoSymbol,
+  portalSymbol,
+  providerSymbol,
+} from './constants';
+import {
+  ReactAnyNode,
+  ReactClassNode,
+  ReactConsumerNode,
+  ReactFragmentNode,
+  ReactFunctionNode,
+  ReactMemoNode,
+  ReactPortalNode,
+  ReactProviderNode,
+} from './types';
+
+export function isFragment(node: ReactAnyNode): node is ReactFragmentNode {
+  return node.$$typeof === elementSymbol && node.type === fragmentSymbol;
+}
+
+export function isClass(node: ReactAnyNode): node is ReactClassNode {
+  return (
+    node.$$typeof === elementSymbol &&
+    (typeof node.type === 'function' &&
+      MATCHES_CLASS.test(Object.toString.call(node.type)))
+  );
+}
+
+export function isFunction(node: ReactAnyNode): node is ReactFunctionNode {
+  return (
+    node.$$typeof === elementSymbol &&
+    (typeof node.type === 'function' &&
+      !MATCHES_CLASS.test(Object.toString.call(node.type)))
+  );
+}
+
+export function isMemo(node: ReactAnyNode): node is ReactMemoNode {
+  return node.$$typeof === memoSymbol;
+}
+
+export function isPortal(node: ReactAnyNode): node is ReactPortalNode {
+  return node.$$typeof === portalSymbol;
+}
+
+export function isProvider(node: ReactAnyNode): node is ReactProviderNode {
+  return node.$$typeof === providerSymbol;
+}
+
+export function isConsumer(node: ReactAnyNode): node is ReactConsumerNode {
+  return node.$$typeof === contextSymbol;
+}
