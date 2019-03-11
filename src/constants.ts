96ef8dccecd468c3d4ef285b8564bc3772f13e74
Jake 'Sid' Smith <jake@dabapps.com>
2019-03-11 22:35:46 +0000

    Add symbols and class regex

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const elementSymbol = Symbol.for('react.element');
+export const memoSymbol = Symbol.for('react.memo');
+export const fragmentSymbol = Symbol.for('react.fragment');
+export const portalSymbol = Symbol.for('react.portal');
+export const providerSymbol = Symbol.for('react.provider');
+export const contextSymbol = Symbol.for('react.context');
+
+export const MATCHES_CLASS = /^class\s/;
