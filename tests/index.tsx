8ed466f22fb7bf5aefb40ae8a97c1edb0497f73c
Jake 'Sid' Smith <jake@dabapps.com>
2019-03-16 02:54:17 +0000

    Move component function tests

--- tests/index.tsx
+++ tests/index.tsx
@@ -4,42 +4,10 @@ import { elementSymbol } from '../src/constants';
 import { compare } from './helpers/compare';
 
 describe('ReactShallowRenderer', () => {
-  const ComponentReturnsArray = ((() => [
-    <p key={1}>First</p>,
-    'Second',
-  ]) as unknown) as React.FunctionComponent; // Casting because this shouldn't really be possible
-
-  const UnknownMemoComponent: React.FunctionComponent = React.memo(() => (
-    <p>Unknown name</p>
-  ));
-
-  const ComponentWithUnknownMemoChild: React.FunctionComponent = () => (
-    <div>
-      <UnknownMemoComponent />
-    </div>
-  );
-
-  const ComponentWithChildren: React.FunctionComponent = ({ children }) => (
-    <p>{children}</p>
-  );
-
   const ComponentWithFalsyChildren: React.FunctionComponent = () => (
     <div>
       <p>{null}</p>
       <p>{false}</p>
-    </div>
-  );
-
-  const MemoComponentWithChildren: React.FunctionComponent = React.memo(
-    ComponentWithChildren
-  );
-
-  const ComponentWithChildrenAndOwnChildren: React.FunctionComponent = ({
-    children,
-  }) => (
-    <div>
-      <p>I have children!</p>
-      {children}
     </div>
   );
 
@@ -59,15 +27,6 @@ describe('ReactShallowRenderer', () => {
       <ComponentWithFragment />
       <MemoComponentWithFragment />
     </div>
-  );
-
-  const MemoComponentWithMemoChildren: React.FunctionComponent = React.memo(
-    ({ children }) => (
-      <div>
-        <MemoComponentWithChildren>I have children!</MemoComponentWithChildren>
-        {children}
-      </div>
-    )
   );
 
   class ComponentClass extends React.Component {
@@ -115,147 +74,6 @@ describe('ReactShallowRenderer', () => {
   );
 
   describe('toJSON', () => {
-    it('renders a basic function component with own and supplied children', () => {
-      const element = (
-        <ComponentWithChildrenAndOwnChildren>
-          <p>I am a child!</p>
-        </ComponentWithChildrenAndOwnChildren>
-      );
-
-      const renderer = new ReactShallowRenderer(element);
-
-      compare(renderer.toJSON(), {
-        $$typeof: elementSymbol,
-        type: 'div',
-        key: null,
-        ref: null,
-        props: {
-          children: [
-            {
-              $$typeof: elementSymbol,
-              type: 'p',
-              key: null,
-              ref: null,
-              props: {
-                children: ['I have children!'],
-              },
-              _owner: null,
-              _store: {},
-            },
-            {
-              $$typeof: elementSymbol,
-              type: 'p',
-              key: null,
-              ref: null,
-              props: {
-                children: ['I am a child!'],
-              },
-              _owner: null,
-              _store: {},
-            },
-          ],
-        },
-        _owner: null,
-        _store: {},
-      });
-    });
-
-    it('renders a memo function component with memo children', () => {
-      const element = (
-        <MemoComponentWithMemoChildren>
-          <p>I am a child!</p>
-        </MemoComponentWithMemoChildren>
-      );
-
-      const renderer = new ReactShallowRenderer(element);
-
-      compare(renderer.toJSON(), {
-        $$typeof: elementSymbol,
-        type: 'div',
-        key: null,
-        ref: null,
-        props: {
-          children: [
-            {
-              $$typeof: elementSymbol,
-              type: 'React.memo(ComponentWithChildren)',
-              key: null,
-              ref: null,
-              props: {
-                children: ['I have children!'],
-              },
-              _owner: null,
-              _store: {},
-            },
-            {
-              $$typeof: elementSymbol,
-              type: 'p',
-              key: null,
-              ref: null,
-              props: {
-                children: ['I am a child!'],
-              },
-              _owner: null,
-              _store: {},
-            },
-          ],
-        },
-        _owner: null,
-        _store: {},
-      });
-    });
-
-    it('renders a memo function component with an unknown name', () => {
-      const element = <ComponentWithUnknownMemoChild />;
-
-      const renderer = new ReactShallowRenderer(element);
-
-      compare(renderer.toJSON(), {
-        $$typeof: elementSymbol,
-        type: 'div',
-        key: null,
-        ref: null,
-        props: {
-          children: [
-            {
-              $$typeof: elementSymbol,
-              type: 'React.memo(Unknown)',
-              key: null,
-              ref: null,
-              props: {
-                children: [],
-              },
-              _owner: null,
-              _store: {},
-            },
-          ],
-        },
-        _owner: null,
-        _store: {},
-      });
-    });
-
-    it('renders a component that returns an array', () => {
-      const element = <ComponentReturnsArray />;
-
-      const renderer = new ReactShallowRenderer(element);
-
-      compare(renderer.toJSON(), [
-        {
-          $$typeof: elementSymbol,
-          type: 'p',
-          key: '1',
-          ref: null,
-          props: {
-            children: ['First'],
-          },
-          _owner: null,
-          _store: {},
-        },
-        'Second',
-      ]);
-    });
-
     it('renders a component class that returns an array', () => {
       const element = <ComponentClassReturnsArray />;
 
