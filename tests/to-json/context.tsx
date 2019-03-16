6a548e5291008e87c5ad46535d3c53c96c74584b
Jake 'Sid' Smith <jake@dabapps.com>
2019-03-16 04:09:59 +0000

    Add tests for rendering context inline

--- tests/to-json/context.tsx
+++ tests/to-json/context.tsx
@@ -19,6 +19,62 @@ describe('ReactShallowRenderer', () => {
   );
 
   describe('toJSON', () => {
+    it('renders an inline provider', () => {
+      const element = (
+        <TestContext.Provider value={{ foo: 'provided value' }}>
+          <ComponentWithConsumer />
+        </TestContext.Provider>
+      );
+
+      const renderer = new ReactShallowRenderer(element);
+
+      compare(renderer.toJSON(), {
+        $$typeof: elementSymbol,
+        type: 'React.Provider',
+        key: null,
+        ref: null,
+        props: {
+          value: {
+            foo: 'provided value',
+          },
+          children: [
+            {
+              $$typeof: elementSymbol,
+              type: 'ComponentWithConsumer',
+              key: null,
+              ref: null,
+              props: {
+                children: [],
+              },
+              _owner: null,
+              _store: {},
+            },
+          ],
+        },
+        _owner: null,
+        _store: {},
+      });
+    });
+
+    it('renders an inline consumer', () => {
+      const element = (
+        <TestContext.Consumer>{ContextRenderer}</TestContext.Consumer>
+      );
+
+      const renderer = new ReactShallowRenderer(element);
+
+      compare(renderer.toJSON(), {
+        $$typeof: elementSymbol,
+        type: 'React.Consumer',
+        key: null,
+        ref: null,
+        props: {
+          children: ['[Function: ContextRenderer]'],
+        },
+        _owner: null,
+        _store: {},
+      });
+    });
     it('renders a component with a provider', () => {
       const element = <ComponentWithProvider />;
 
